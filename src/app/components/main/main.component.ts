690ff81f140be5a7ecadc14c0603201d842374f5
shimaenaga <admin@shimaenaga.dev>
2025-06-19 22:20:54 +0900

    Add Profile Icons

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -7,6 +7,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatChipsModule } from '@angular/material/chips';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faDiscord, faGithub} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-main-component',
@@ -19,10 +21,14 @@ import { MatChipsModule } from '@angular/material/chips';
     MatGridListModule,
     MatProgressBarModule,
     MatChipsModule,
+    FaIconComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
 export class MainComponent {
   private router = inject(Router);
+  protected readonly faGithub = faGithub;
+  protected readonly faDiscord = faDiscord;
+  protected readonly location = location;
 }
